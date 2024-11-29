8bfc88c3c74ebda10d73f0e865f6404da9f7ccd9
DESKTOP-JP71VLA\USER <kiet.trinh8804@hcmut.edu.vn>
2024-11-29 14:06:12 +0700

    init Lab4

---
+++ LAB4_Exercise/Core/Inc/input_processing.h
@@ -0,0 +1,24 @@
+/*
+ * input_processing.h
+ *
+ *  Created on: Nov 29, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+
+#include "main.h"
+#include "sched.h"
+#include "global.h"
+#include "traffic_light.h"
+
+void init_buffer();
+void button_reading(void);
+unsigned char is_button_pressed(unsigned char index);
+unsigned char is_button_pressed_1s(unsigned char index);
+unsigned char is_button_held(unsigned char index);
+void reset_flagForButtonHold(unsigned char index);
+
+#endif /* INC_INPUT_READING_H_ */
+
